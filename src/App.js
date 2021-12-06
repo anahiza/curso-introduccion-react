e558ce0141c382cd21d208b9ca5806ff74fb6d4e
Anahi Zentella A <anahizentella@clandbus.com>
2021-12-06 11:18:26 -0600

    App maquetada

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react'
+import './App.css';
+import {TodoCounter} from './TodoCounter'
+import { TodoList } from './TodoList';
+import { TodoSearch } from './TodoSearch';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+
+const todos=[
+  {text:"Cortar cebolla", completed:false},
+  {text:"Curso platzi", completed:false},
+  {text:"Cortar cebolla", completed:false},
+]
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+    <TodoCounter />
+    <TodoSearch />
+
+    <TodoList>
+      {todos.map(todo => (
+        <TodoItem
+          key={todo.text}
+          text={todo.text}
+          completed={todo.completed}
+        />
+      ))}
+    </TodoList>
+
+    <CreateTodoButton />
+  </React.Fragment>
+  );     
 }
 
 export default App;
