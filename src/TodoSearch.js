4c87d6548deaa4e38a8b7bedf95657a6c0543673
Anahi Zentella A <anahizentella@clandbus.com>
2021-12-10 11:20:36 -0600

    manejo del estado

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,13 +2,23 @@ import React from 'react';
 import "./TodoSearch.css"
 
 function TodoSearch() {
+  const [saerchValue, setSearchValue]=React.useState('')
+
   const onSearchValueChange=(event)=>{
-    console.log(event.target.value);
+    setSearchValue(event.target.value)
   }
   
-  return (
-    <input placeholder="Cebolla" className="TodoSearch" onChange={onSearchValueChange}/>
-  );
+  return [
+      <input 
+      placeholder="Cebolla" 
+      className="TodoSearch" 
+      onChange={onSearchValueChange} 
+      value={saerchValue}/>,
+      <p>{saerchValue}</p>
+      
+
+  ];
+  
 }
 
 export { TodoSearch };
